73ed9da13f627a0d02375b0d171849c10ad463e5
thomasrobiglio <robigliothomas@gmail.com>
2026-08-05 17:00:55 +0200

    fix button and minor cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Paradigm Shift by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -34,12 +28,6 @@
 	// Mobile?
 		if (browser.mobile)
 			$body.addClass('is-mobile');
-
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
-			});
 
 	// Polyfill: Object fit.
 		if (!browser.canUse('object-fit')) {
